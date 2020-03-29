e883f2715813dcc1bca8c8a7ac380df3de9d1d16
wymerkd <wymerkd@gmail.com>
2020-03-29 11:30:25 -0700

    add onload evenet listener

--- js/index.js
+++ js/index.js
@@ -62,10 +62,13 @@ let todoList = {
   }
 };
 
-let displayTodosButton = document.getElementById('displayTodosButton');
-console.log(displayTodosButton);
+document.addEventListener("DOMContentLoaded", function () {
+  let displayTodosButton = document.getElementById('displayTodosButton');
+  console.log(displayTodosButton);
 
-displayTodosButton.addEventListener('click', function() {
-  todoList.displayTodos();
+  displayTodosButton.addEventListener('click', function () {
+    todoList.displayTodos();
+  });
 });
+
 
